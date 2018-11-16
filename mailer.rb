7eb05413fdb7192085eb4dbee535290af851ae64
Tate Galbraith <tateg@users.noreply.github.com>
2018-11-15 17:56:30 -0800

    add_mailer_timestamp (#6)
    This change adds a timestamp to the bottom of the email to prevent gmail
    from trimming output and also show when the alert was generated on
    system.

--- mailer.rb
+++ mailer.rb
@@ -5,6 +5,7 @@ require 'action_mailer'
 class Mailer < ActionMailer::Base
   def notification(to:, from:, subject:, worker_diff:)
     @worker_diff = worker_diff
+    @footer_timestamp = Time.now.strftime("%m/%d/%Y%l:%M%p %Z") # timestamp bottom of email to prevent trimming
     mail(to: to, from: from, subject: subject) do |format|
       format.text
       format.html

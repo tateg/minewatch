a43b9e1154040b3d9939e87b5bf128005ad0aa2a
tateg <tate.galbraith@gmail.com>
2018-11-19 19:46:12 -0800

    rubocop_and_code_climate
    This change adds a rubocop yml configuration and makes all advised
    rubocop corrections based on the initial linting.
    
    This change also adds Code Climate badges to the readme.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # MineWatch
+<a href="https://codeclimate.com/github/tateg/minewatch/maintainability"><img src="https://api.codeclimate.com/v1/badges/28b9dbff3eb96be18337/maintainability" /></a>
+<a href="https://codeclimate.com/github/tateg/minewatch/test_coverage"><img src="https://api.codeclimate.com/v1/badges/28b9dbff3eb96be18337/test_coverage" /></a>
 MineWatch is an application used to monitor custom cryptocurrency miners and report on their current status. The application can be configured to send alerts when workers go offline and provide a daily summary of work for each miner. The application is written entirely in ruby and leverages ActionMailer for sending email alerts and Rufus Scheduler for scheduling alerts and emails. The application includes a basic systemd service configuration.
 
 The application is built with support for the Bitfly API used for ETH, ETC and ZEC mining pools and should be interchangeable. An example of the API can be found at: https://ethermine.org/api/
--- mine_watch_runner.rb
+++ mine_watch_runner.rb
@@ -13,28 +13,27 @@ require_relative 'mailer'
 ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-   :address        => "smtp.gmail.com",
-   :port           => 587,
-   :domain         => "example.com",
-   :authentication => :plain,
-   :user_name      => ENV['EMAIL_USERNAME'],
-   :password       => ENV['EMAIL_PASSWORD'],
-   :enable_starttls_auto => true
-  }
+  address: 'smtp.gmail.com',
+  port: 587,
+  domain: 'example.com',
+  authentication: :plain,
+  user_name: ENV['EMAIL_USERNAME'],
+  password: ENV['EMAIL_PASSWORD'],
+  enable_starttls_auto: true
+}
 ActionMailer::Base.view_paths = File.dirname(__FILE__)
 scheduler = Rufus::Scheduler.new
-minewatch = MineWatch.new({ pool_api_url: ENV['ETH_POOL_API_URL'],
-                            addr: ENV['ETH_ADDR'],
-                            workers: ENV['WORKERS'].to_i
-})
-
+minewatch = MineWatch.new(pool_api_url: ENV['ETH_POOL_API_URL'],
+                          addr: ENV['ETH_ADDR'],
+                          workers: ENV['WORKERS'].to_i)
 worker_alert_count = 0
 
 # 5 Minute Online Workers Test
 scheduler.every '5m' do
   workers_online = minewatch.all_workers_online?
   worker_diff = "#{minewatch.current_active_workers}/#{ENV['WORKERS']} Workers Online"
-  mailer = Mailer.notification(to: ENV['EMAIL_TO'], from: ENV['EMAIL_FROM'], subject: ENV['EMAIL_SUBJECT_ALERT'], view_args: { worker_diff: worker_diff })
+  view_args = { worker_diff: worker_diff }
+  mailer = Mailer.notification(to: ENV['EMAIL_TO'], from: ENV['EMAIL_FROM'], subject: ENV['EMAIL_SUBJECT_ALERT'], view_args: view_args)
   if !workers_online
     if worker_alert_count > 3
       mailer.deliver if (worker_alert_count % 12).zero? # only alert every hour after 3 alerts in succession
@@ -56,7 +55,7 @@ scheduler.cron '0 18 * * *' do
                     usd_per_month: minewatch.usd_per_month,
                     worker_diff: worker_diff }
   mailer = Mailer.daily_summary(to: ENV['EMAIL_TO'], from: ENV['EMAIL_FROM'], subject: ENV['EMAIL_SUBJECT_SUMMARY'], view_args: summary_stats)
-  mailer.deliver  
+  mailer.deliver
 end
 
 scheduler.join
